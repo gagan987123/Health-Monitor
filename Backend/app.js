136d86d45556d992f5926324c85debf7ffc0276e
Singh <gagandeep.singh@invenco.com>
2025-11-12 13:28:07 +0530

    mail-setup

--- Backend/app.js
+++ Backend/app.js
@@ -74,8 +74,8 @@ const transporter = nodemailer.createTransport({
     port: 587,
     secure: false, // Use TLS
     auth: {
-        user: 'health987123@gmail.com',
-        pass: "ildz eabx jppi zmfv"
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASS
     }
 });
 
